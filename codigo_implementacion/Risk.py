204f02a86b201740e8bc9d92b4269ac2cc3a6ef5
noatino <paranoid_14@hotmail.com>
2014-11-13 14:21:24 -0600

    continue

---
+++ codigo_implementacion/Risk.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+# -*- coding: utf-8 _*_
+
+from readXML import *
+
+territorio = territorio()
+territorio.getpais('Venezuela').tostring()
+
+territorio.getInformacionVecinos('Venezuela')
--- codigo_implementacion/readXML.py
+++ codigo_implementacion/readXML.py
@@ -4,35 +4,48 @@
 import xml.etree.ElementTree as ET
 from Node import *
 
-###########################################
-# Por medio de este modulo es que podemos #
-# generar un parser para un archivo xml   #
-# y cargar la grafica de nuestro          #
-# territorio de juego.                    #
-# Fernando Abigail Galicia Mendoza        #
-# Estefania Prieto Larios                 #
-# Antonio Galvan                          #
-###########################################
-
-def loadXML():
-    '''
-    Metodo encargado de generar el arbol que 
-    se encuentra definido en el archivo .xml
-    '''
-    territorio = ET.parse('Territorio.xml')
-    arbol = territorio.getroot()
-
-    listOfNodes = []
-
-    for node in arbol:
-        vecinos = [x.text for x in node.findall('vecino')]
-        nodo = Node(node.attrib['nombre'], node.find('id').text, 
-                    node.find('continente').text,node.find('tropas').text,
-                    node.find('jugador').text,vecinos)
-
-        listOfNodes.append(nodo)
-
-    for elem in listOfNodes:
-        elem.tostring()
-
-loadXML()
+class territorio:
+    
+    ###########################################
+    # Por medio de este modulo es que podemos #
+    # generar un parser para un archivo xml   #
+    # y cargar la grafica de nuestro          #
+    # territorio de juego.                    #
+    # Fernando Abigail Galicia Mendoza        #
+    # Estefania Prieto Larios                 #
+    # Antonio Galvan                          #
+    ###########################################
+    
+    def __init__(self):
+        '''
+        Metodo encargado de generar el arbol que 
+        se encuentra definido en el archivo .xml
+        '''
+        self.listOfNodes = []
+        territorio = ET.parse('Territorio.xml')
+        arbol = territorio.getroot()
+        
+        for node in arbol:
+            vecinos = [x.text for x in node.findall('vecino')]
+            nodo = Node(node.attrib['nombre'], node.find('id').text, 
+                        node.find('continente').text,
+                        node.find('tropas').text,
+                        node.find('jugador').text,vecinos)
+            
+            self.listOfNodes.append(nodo)
+            
+    def getpais(self, nombre):
+        for pais in self.listOfNodes:
+            if pais.getnombre() == nombre:
+                return pais
+            else:
+                continue
+        print 'Pais no valido dentro del territorio de juego'
+        return None
+    
+        
+    def getInformacionVecinos(self, pais):
+        '''Devuelve la información de los vecinos del pais dado'''
+        espia = [vecinos for vecinos in ]
+        print espia
+        

6e0dc537f03df1421d408dd8ae36ee1006c29425
faniaP <faniarinoa@gmail.com>
2014-11-13 19:53:31 -0600

    now its working

---
+++ codigo_implementacion/Risk.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+# -*- coding: utf-8 _*_
+
+from readXML import *
+from acciones import *
+
+territorio = territorio()
+territorio.getpais('Venezuela').tostring()
+
+#print territorio.getvecinos('Venezuela')
+
+#print territorio.getinfovecinos('Venezuela')
+
+territorio.settropasterritorio('Venezuela', 50)
+
+territorio.getpais('Venezuela').tostring()
+
+print territorio.getinfovecinos('Brasil')
+
